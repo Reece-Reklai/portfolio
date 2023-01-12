c46fe1f50ddd3f34c396c51c5e9081ba7da71eb0
REECE P REKLAI <official.reklai@gmail.com>
2023-01-12 13:10:20 -0800

    PROGRESSION

--- my-application/src/components/Project.js
+++ my-application/src/components/Project.js
@@ -27,41 +27,6 @@ const Project = () => {
       <CssBaseline />
       <Container maxWidth="xl" className="container">
         <Box sx={{ flexGrow: 1 }}>
-          <Grid
-            container
-            spacing={{ xs: 2, md: 3 }}
-            columns={{ xs: 4, sm: 8, md: 12 }}
-          >
-            {Array.from(Array(6)).map((_, index) => (
-              <Grid item xs={2} sm={4} md={4} key={index}>
-                <Item>
-                  {" "}
-                  <Card sx={{ maxWidth: 345 }}>
-                    <CardMedia
-                      component="img"
-                      alt="green iguana"
-                      height="140"
-                      image="/static/images/cards/contemplative-reptile.jpg"
-                    />
-                    <CardContent>
-                      <Typography gutterBottom variant="h5" component="div">
-                        Lizard
-                      </Typography>
-                      <Typography variant="body2" color="text.secondary">
-                        Lizards are a widespread group of squamate reptiles,
-                        with over 6,000 species, ranging across all continents
-                        except Antarctica
-                      </Typography>
-                    </CardContent>
-                    <CardActions>
-                      <Button size="small">Share</Button>
-                      <Button size="small">Learn More</Button>
-                    </CardActions>
-                  </Card>
-                </Item>
-              </Grid>
-            ))}
-          </Grid>
         </Box>
       </Container>
     </div>
